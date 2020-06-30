9ac1ce91c66d550fe9783009bf9ed01b7d2f3acb
kunkka <hawtim_zhang@qq.com>
2020-06-30 10:29:05 +0800

    feat: load elementui components when require

---
+++ src/plugins/element.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import {
+  Button,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  DatePicker,
+  Form,
+  FormItem,
+  Dialog,
+  Input,
+  Select,
+  Option
+} from 'element-ui'
+
+Vue.use(Button)
+  .use(Row)
+  .use(Col)
+  .use(Table)
+  .use(TableColumn)
+  .use(DatePicker)
+  .use(Form)
+  .use(FormItem)
+  .use(Dialog)
+  .use(Input)
+  .use(Select)
+  .use(Option)
