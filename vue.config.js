d70b32ed2030f28f07f729bb9252fa7a4b3da670
kunkka <hawtim_zhang@qq.com>
2020-07-02 17:21:56 +0800

    build: define webpack providePlugin

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,19 @@
+const webpack = require('webpack')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 
 module.exports = {
+  chainWebpack: config => {
+    // 使用 provideplugin 注入全局模块变量
+    config.plugin('provide').use(webpack.ProvidePlugin, [
+      {
+        mapActions: ['vuex', 'mapActions'],
+        mapGetters: ['vuex', 'mapGetters'],
+        mapMutations: ['vuex', 'mapMutations'],
+        mapState: ['vuex', 'mapState']
+      }
+    ])
+  },
   configureWebpack: {
     module: {
       rules: [
