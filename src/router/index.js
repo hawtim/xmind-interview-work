0d475ce8b9d29b86d8efe811e847ff07797002cc
kunkka <hawtim_zhang@qq.com>
2020-07-02 17:18:11 +0800

    fire: invalid route page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Summary from '../views/Summary.vue'
 
 Vue.use(VueRouter)
 
@@ -10,20 +9,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/summary',
-    name: 'Summary',
-    component: Summary
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   // 重定向处理
   {
