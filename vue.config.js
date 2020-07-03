abca739e3efad5d3c2aa43d50d7b62552c8a2211
kunkka <hawtim_zhang@qq.com>
2020-07-03 11:57:20 +0800

    lint: format

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,16 @@ const report = process.env.npm_config_report;
 module.exports = {
   chainWebpack: config => {
     // 使用 provideplugin 注入全局模块变量
-    config.plugin('provide').use(webpack.ProvidePlugin, [
-      {
-        mapActions: ['vuex', 'mapActions'],
-        mapGetters: ['vuex', 'mapGetters'],
-        mapMutations: ['vuex', 'mapMutations'],
-        mapState: ['vuex', 'mapState']
-      }
-    ])
+    config
+      .plugin('provide')
+      .use(webpack.ProvidePlugin, [
+        {
+          mapActions: ['vuex', 'mapActions'],
+          mapGetters: ['vuex', 'mapGetters'],
+          mapMutations: ['vuex', 'mapMutations'],
+          mapState: ['vuex', 'mapState']
+        }
+      ])
     if (report) {
       config
         .plugin('webpack-bundle-analyzer')
