a1a5e052d0761a51b7f151bc7d61728d3fa2ac45
kunkka <hawtim_zhang@qq.com>
2020-07-03 11:56:18 +0800

    fire: remove coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,5 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy'
-  },
-  "collectCoverage": true,
-  "collectCoverageFrom": ["**/*.{js,vue}", "!**/node_modules/**"]
+  }
 }
